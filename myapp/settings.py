a0a3aeaab5e7cc79e8dfa3313d268b89eb547d43
saki <st374md@gmail.com>
2020-02-23 02:38:44 +0000

    deployeeeeeeee

--- myapp/settings.py
+++ myapp/settings.py
@@ -105,7 +105,7 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 #DATABASES = {
-    'default': {
+     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
