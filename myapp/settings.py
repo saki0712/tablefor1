304d41843ebdf96402ad1df934c51a446b1aa470
saki <st374md@gmail.com>
2020-02-24 06:57:51 +0000

    committt!

--- myapp/settings.py
+++ myapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import os
 
+# DB heroku
 import dj_database_url
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 db_from_env = dj_database_url.config()
@@ -29,32 +30,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY='g*k*&9-gi%e=s#qx!40ew-vef62vegi)!hq62qs_!(n72!)a^e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
-        
-
-
-ALLOWED_HOSTS = ['*']
-
+#DEBUG = False
 
 
 # Application definition
@@ -103,11 +82,6 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 LOGIN_REDIRECT_URL = '/'
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -147,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-SECRET_KEY='g*k*&9-gi%e=s#qx!40ew-vef62vegi)!hq62qs_!(n72!)a^e'
+
 
 from socket import gethostname
 hostname = gethostname()
@@ -157,11 +131,15 @@ if "saki-1" in hostname:
     DEBUG = True
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'mealmapdb',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
         }
     }
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['*']
 
 else:
     
