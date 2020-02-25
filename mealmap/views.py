35bf3b332fd6e6d858142150cff0bb90dc0e5c14
saki <st374md@gmail.com>
2020-02-25 04:07:16 +0000

    modification

--- mealmap/views.py
+++ mealmap/views.py
@@ -2,6 +2,7 @@ from django.views import generic
 from .models import Category, Shop
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 
 class IndexView(generic.ListView):
     model = Shop
@@ -11,11 +12,21 @@ class DetailView(generic.DetailView):
     
 class UpdateView(LoginRequiredMixin, generic.edit.UpdateView):
     model = Shop
-    fields = '__all__'
+    fields = ['name', 'address', 'category']
+    
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied('You do not have permission to edit.')
+        return super(UpdateView, self).dispatch(request, *args, **kwargs)
     
 class CreateView(LoginRequiredMixin, generic.edit.CreateView):
     model = Shop
-    fields = '__all__'
+    fields = ['name', 'address', 'category']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(CreateView, self).form_valid(form)
     
 class DeleteView(LoginRequiredMixin, generic.edit.DeleteView):
     model = Shop
